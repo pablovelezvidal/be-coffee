a2bf8e02bace345c16943130fa62ad6e04ac7c0d
Pablo Velez <Pablovelez@AL-COL-02313.local>
2018-03-19 21:17:25 -0500

    removing unnecesary code

--- index.js
+++ index.js
@@ -18,7 +18,3 @@ app.use('/', restApi);
 
 app.listen(3000);
 console.log('Listening to port:  ' + 3000);
-
-// app.listen(3000, '192.168.0.25', function() {
-//     console.log('Listening to port:  ' + 3000);
-// });
